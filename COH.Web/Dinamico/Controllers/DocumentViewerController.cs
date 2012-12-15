488ef0abdecb08de0bcee2c987a4f50f014f7609
Ben Herila <ben@herila.net>
2012-12-14 21:40:30 -0800

    added Dinamico folder

---
+++ COH.Web/Dinamico/Controllers/DocumentViewerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using N2.Web;
+using N2.Web.Mvc;
+
+namespace Dinamico.Controllers
+{
+
+    [Controls(typeof(Models.DocumentViewer))]
+    public class DocumentViewerController : ContentController<Models.DocumentViewer>
+    {
+
+        public override ActionResult Index()
+        {
+            return View(CurrentItem.TemplateKey, CurrentItem);
+        }
+
+    }
+}
---
+++ COH.Web/Dinamico/Controllers/RedirectController.cs
@@ -0,0 +1,18 @@
+﻿using N2.Web;
+using N2.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dinamico.Controllers
+{
+    [Controls(typeof(Models.Redirect))]
+    public class LinkController : ContentController<Models.Redirect>
+    {
+        public override System.Web.Mvc.ActionResult Index()
+        {
+            return View(new string[] { CurrentItem.Link });
+        }
+    }
+}
